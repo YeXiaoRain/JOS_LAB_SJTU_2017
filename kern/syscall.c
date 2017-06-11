669f76195cd52758af5ad2c0f457dc6bfab87ae5
yexiaorain <yexiaorain@gmail.com>
2017-06-10 22:29:08 -0400

    lab6 PartA (1/5)

--- kern/syscall.c
+++ kern/syscall.c
@@ -399,8 +399,7 @@ syscall_wrapper(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint3
 static int
 sys_time_msec(void)
 {
-	// LAB 6: Your code here.
-	panic("sys_time_msec not implemented");
+  return time_msec();
 }
 
 
@@ -446,6 +445,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
       return sys_ipc_try_send((envid_t)a1, (uint32_t)a2, (void *)a3, (unsigned)a4);
     case SYS_ipc_recv:
       return sys_ipc_recv((void *)a1);
+    case SYS_time_msec:
+      return sys_time_msec();
     case NSYSCALLS:
     default:
       return -E_INVAL;
